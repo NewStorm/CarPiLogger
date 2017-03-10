89e86085f862f27e0aac0ee276a1c839c383fd33
BigStorm <michi11724dev@googlemail.com>
2017-03-10 14:03:30 +0100

    -added Logfile

--- CarPiLogger/systemlogger.py
+++ CarPiLogger/systemlogger.py
@@ -4,14 +4,16 @@ import time
 import datetime
 
 #Variables
-LOGFILE_PATH = "/Users/gymnasiumundtechnikteam/Desktop/logfile.csv"
+LOGFILE_RELATIVE_PATH = "/logfile.csv"
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+print(CURRENT_PATH)
 
 #Get CPU Temperature
 def getCPUtemperature():  
     return os.popen('vcgencmd measure_temp').readline()  
 
 #Load Files
-loggingfile = open(LOGFILE_PATH, "a")
+loggingfile = open(CURRENT_PATH + LOGFILE_RELATIVE_PATH, "a")
 
 new_data = datetime.datetime.now().strftime("%d.%m.%y;%H:%M:%S")
 
